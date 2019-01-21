67a068919b59eff50a6787a2fac032630880818c
fierd <daniel.fierro@mitechnologiesing.com>
2019-01-21 12:11:22 -0800

    Se comenzo con la vista del login

---
+++ MovileApp/platforms/browser/www/js/App/Login.js
@@ -0,0 +1,6 @@
+var app = angular.module('Login',[]);
+app.controller('LoginCtrl', function ($scope) {
+   
+    $scope.Msg="Angular js Say Hello";
+
+});
---
+++ MovileApp/www/js/App/Login.js
@@ -0,0 +1,7 @@
+var app = angular.module('Login',['onsen']);
+
+// Start Login Controller
+app.controller('LoginCtrl', function ($scope) {
+
+});
+// End Login controller
