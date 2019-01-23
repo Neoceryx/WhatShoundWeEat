8fe1ab450b8341dfb44603db15978e7f40657324
fierd <daniel.fierro@mitechnologiesing.com>
2019-01-23 11:24:14 -0800

    Se comenzo con la vista del dashboard

--- MovileApp/www/js/App/DashBoard.js
+++ MovileApp/www/js/App/DashBoard.js
@@ -1,13 +1,24 @@
 var app = angular.module("DashApp",[]);
 
 // start Dashboard controller controller
-app.controller("DashCtrl",function ($scope) {
+app.controller("DashCtrl",function ($scope, $http) {
+
+    // Set Apis EndPoint
+    const SERVER="http://192.168.0.65/";
 
     // recover user infor from local storage
     var UserData = JSON.parse(localStorage.getItem("UserInfo"));
     debugger
+
     function GetMyGroups() {
         
+        // Start http request to get all muy groups
+        $http({
+            method:"GET",
+            url: SERVER+""
+        })
+        // Start http request to get all muy groups    
+
     }
 
     
---
+++ SWETAPIS/SWETAPIS/Models/GroupRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SWETAPIS.Models
+{
+    public class GroupRepository
+    {
+        #region Variables
+        // create context Object
+        WSWETEntities _context = new WSWETEntities();
+        #endregion
+
+    }
+}
